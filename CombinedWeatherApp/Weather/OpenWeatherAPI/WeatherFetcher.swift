d8bc0d0cf069c12136712e72a413550107de8056
HiramCM <91649588+HiramCM@users.noreply.github.com>
2021-10-14 13:47:57 -0700

    improvements to API section
    Adding code to fetch from API using combine

--- CombinedWeatherApp/Weather/OpenWeatherAPI/WeatherFetcher.swift
+++ CombinedWeatherApp/Weather/OpenWeatherAPI/WeatherFetcher.swift
@@ -29,6 +29,11 @@
 import Foundation
 import Combine
 
+protocol WeatherFetchable {
+  func weeklyWeatherForecast(forCity city:String) -> AnyPublisher<WeeklyForecastResponse, WeatherError>
+  func currentWeatherForecast(forCity city:String) -> AnyPublisher<CurrentWeatherForecastResponse, WeatherError>
+}
+
 class WeatherFetcher {
   private let session: URLSession
   
@@ -81,4 +86,37 @@ private extension WeatherFetcher {
     
     return components
   }
+}
+
+extension WeatherFetcher: WeatherFetchable {
+  
+  func weeklyWeatherForecast(forCity city: String) -> AnyPublisher<WeeklyForecastResponse, WeatherError> {
+    return forecast(with: makeWeeklyForecastComponents(withCity: city))
+  }
+  
+  func currentWeatherForecast(forCity city: String) -> AnyPublisher<CurrentWeatherForecastResponse, WeatherError> {
+    return forecast(with: makeCurrentDayForecastComponents(withCity: city))
+  }
+  
+  private func forecast<T>(with components: URLComponents) -> AnyPublisher<T, WeatherError> where T:Decodable {
+    // 1
+    guard let url = components.url else {
+      let error = WeatherError.network(description: "Couldn't create URL")
+      return Fail(error: error).eraseToAnyPublisher()
+    }
+    
+    // 2
+    return session.dataTaskPublisher(for: URLRequest(url: url))
+    // 3
+      .mapError { error in
+        .network(description: error.localizedDescription)
+      }
+    // 4
+      .flatMap(maxPublishers: .max(1)) { pair in
+        decode(pair.data)
+      }
+    // 5
+      .eraseToAnyPublisher()
+  }
+  
 }
